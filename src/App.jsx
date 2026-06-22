d69a4a69fee645d7a838ab67c832af726fe90ead
JOGOS <leonardoarthur380@gmail.com>
2026-06-22 13:34:38 -0300

    SDSF

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function AdminLayout() {
 
         <nav>
           <Link to="/">
-            <LayoutDashboard size={20} /> Dashboard
+            <LayoutDashboard size={20} /> DasDDhboard
           </Link>
 
           <Link to="/importacao">
